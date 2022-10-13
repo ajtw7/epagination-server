1bea12e5d668d9d4893ab9c9b20d3f0aeddb0440
Andrew <ayjtw7@gmail.com>
2022-10-13 13:10:42 -0400

    rendering wishlist on profile

--- routes/users.js
+++ routes/users.js
@@ -9,101 +9,129 @@ router.get("/", (req, res, next) => {
   res.send("respond with a resource");
 });
 router.post("/signup", (req, res, next) => {
-      if (!req.body.username || !req.body.password) {
+  if (!req.body.username || !req.body.password) {
+    return res.status(400).json({
+      message: "please fill out both fields",
+    });
+  }
+  User.findOne({
+    username: req.body.username,
+  })
+    .then((foundUser) => {
+      if (foundUser) {
         return res.status(400).json({
-          message: "please fill out both fields"
+          message: "Username is already taken",
         });
-      }
-      User.findOne({
-          username: req.body.username
+      } else {
+        const salt = bcrypt.genSaltSync(saltRounds);
+        const hashedPass = bcrypt.hashSync(req.body.password, salt);
+        User.create({
+          username: req.body.username,
+          password: hashedPass,
+          email: req.body.email,
         })
-        .then((foundUser) => {
-          if (foundUser) {
-            return res.status(400).json({
-                message: "Username is already taken" 
-              });
-              } else {
-                const salt = bcrypt.genSaltSync(saltRounds);
-                const hashedPass = bcrypt.hashSync(req.body.password, salt);
-                User.create({
-                    username: req.body.username,
-                    password: hashedPass,
-                    email: req.body.email
-                  })
-                  .then((createdUser) => {
-                    const payload = {
-                      _id: createdUser._id
-                    };
-                    const token = jwt.sign(payload, process.env.SECRET, {
-                      algorithm: "HS256",
-                      expiresIn: "24hr",
-                    });
-                    res.json({
-                      token: token,
-                      id: createdUser._id
-                    });
-                  })
-                  .catch((err) => {
-                    res.status(400).json(err.message);
-                  });
-              }
-            })
+          .then((createdUser) => {
+            const payload = {
+              _id: createdUser._id,
+            };
+            const token = jwt.sign(payload, process.env.SECRET, {
+              algorithm: "HS256",
+              expiresIn: "24hr",
+            });
+            res.json({
+              token: token,
+              id: createdUser._id,
+            });
+          })
           .catch((err) => {
             res.status(400).json(err.message);
           });
+      }
+    })
+    .catch((err) => {
+      res.status(400).json(err.message);
+    });
+});
+router.post("/login", (req, res, next) => {
+  if (!req.body.username || !req.body.password) {
+    return res.status(400).json({
+      message: "please fill out both fields ",
+    });
+  }
+  User.findOne({
+    username: req.body.username,
+  })
+    .then((foundUser) => {
+      if (!foundUser) {
+        return res
+          .status(401)
+          .json({ message: "Username or Password is incorrect!!!" });
+      }
+      const doesMatch = bcrypt.compareSync(
+        req.body.password,
+        foundUser.password
+      );
+      if (doesMatch) {
+        const payload = { _id: foundUser._id, username: foundUser.username };
+        const token = jwt.sign(payload, process.env.SECRET, {
+          algorithm: "HS256",
+          expiresIn: "24hr",
         });
-      router.post("/login", (req, res, next) => {
-            if (!req.body.username || !req.body.password) {
-              return res.status(400).json({
-                    message: "please fill out both fields " });
-                  }
-                  User.findOne({
-                    username: req.body.username
-                  })
-                  .then((foundUser) => {
-                    if (!foundUser) {
-                      return res.status(401).json({ message: "Username or Password is incorrect!!!" });
-                    }
-                    const doesMatch = bcrypt.compareSync(
-                      req.body.password,
-                      foundUser.password
-                    );
-                    if (doesMatch) {
-                      const payload = { _id: foundUser._id, username: 
-              foundUser.username };
-                      const token = jwt.sign(payload, process.env.SECRET, {
-                        algorithm: "HS256",
-                        expiresIn: "24hr",
-                      });
-                      res.json({ token: token, id: foundUser._id, message: `Welcome $
-              {foundUser.username}` });
-                    } else {
-                      return res.status(402).json({ message: "Username or Password is incorrect" });
-                    }
-                  })
-                  .catch((err) => {
-                    res.json(err.message);
-                  });
-              });
-              router.get("/login-test", isLoggedIn, (req, res) => {
-                return res.status(200).json(req.user);
-              });
-              router.post("/delete-user", isLoggedIn, (req, res, next) => {
-                User.findById(req.user._id)
-                  .then((foundUser) => {
-                    const doesMatch = bcrypt.compareSync(
-                      req.body.password,
-                      foundUser.password
-                    );
-                    if (doesMatch) {
-                      foundUser.delete();
-                      res.json({ message: "success" });
-                    } else {
-                      res.status(401).json({ message: "password doesn't match" });
-                    }
-                  })
-                  .catch((error) => {
-                    res.status(400).json(error.message);
-                  });
-              });
-              module.exports = router;
+        res.json({
+          token: token,
+          id: foundUser._id,
+          message: `Welcome $
+              {foundUser.username}`,
+        });
+      } else {
+        return res
+          .status(402)
+          .json({ message: "Username or Password is incorrect" });
+      }
+    })
+    .catch((err) => {
+      res.json(err.message);
+    });
+});
+router.get("/login-test", isLoggedIn, (req, res) => {
+  return res.status(200).json(req.user);
+});
+router.post("/delete-user", isLoggedIn, (req, res, next) => {
+  User.findById(req.user._id)
+    .then((foundUser) => {
+      const doesMatch = bcrypt.compareSync(
+        req.body.password,
+        foundUser.password
+      );
+      if (doesMatch) {
+        foundUser.delete();
+        res.json({ message: "success" });
+      } else {
+        res.status(401).json({ message: "password doesn't match" });
+      }
+    })
+    .catch((error) => {
+      res.status(400).json(error.message);
+    });
+});
+
+router.get("/profile", isLoggedIn, (req, res, next) => {
+  User.find({
+    _id: req.user._id,
+  })
+    .populate({
+      path: "wishlist"
+    })
+    .populate({
+      path: "finishedBooks"
+    })
+
+    .then((foundUser) => {
+      res.json(foundUser)
+      console.log(foundUser)
+
+      // res.render('users/user-profile', {thisUser: thisUser})
+    })
+    .catch((err) => console.log(err));
+});
+module.exports = router;
--- routes/wishlist.routes.js
+++ routes/wishlist.routes.js
@@ -1,58 +1,61 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const isLoggedIn = require('../middleware/isLoggedIn');
-const User = require('../models/User.model');
-const WishlistBook = require('../models/WishlistBook.model');
+const isLoggedIn = require("../middleware/isLoggedIn");
+const User = require("../models/User.model");
+const WishlistBook = require("../models/WishlistBook.model");
 
+router.post("/addToWishlist", isLoggedIn, (req, res, next) => {
+  const { bookTitle, authorName, bookId } = req.body;
 
-router.post('/addToWishlist', isLoggedIn, (req, res, next) => {
-    
-    const {bookTitle, authorName, bookId } = req.body
-
-    WishlistBook.create({
-        bookTitle,
-        authorName,
-        bookId
-    })
-    .then(newWLBook => {
-        console.log(newWLBook)
-        User.findByIdAndUpdate(
-            { _id: req.user._id}, 
-            { $addToSet: {wishlist: newWLBook}}
-            )
-            .then((result) => {
-                console.log(result)
-            })
-            .cath((err) => {
-                console.log(err)
-            })
-        //     {
-        //         $addToSet: {watchlistBooks: newWLBook._id}
-        //     }, { new: true })
+  WishlistBook.create({
+    bookTitle,
+    authorName,
+    bookId,
+  })
+    .then((newWLBook) => {
+      console.log(newWLBook);
+      User.findByIdAndUpdate(
+        { _id: req.user._id },
+        { $addToSet: { wishlist: newWLBook } }
+      )
+        .then((result) => {
+          console.log(result);
+        })
+        .cath((err) => {
+          console.log(err);
+        });
+      //     {
+      //         $addToSet: {watchlistBooks: newWLBook._id}
+      //     }, { new: true })
     })
     // .then(updatedUser => {
     //     console.log('updated user', updatedUser)
     //     // res.redirect('/profile')
     //     res.json({ message: 'New wishlist book added to user.' })
-        
+
     // })
     .catch((err) => {
-        console.log(err)
+      console.log(err);
+    });
+
+  // router.get('/profile', (req, res, next) => {
+  //     const wlBookID = req.params.
+
+  //     WishlistBook.findByIdAndDelete()
+  //     .then(deletedBook => {
+  //         console.log(deletedBook)
+  //         res.redirect('/profile')
+  //     })
+  // })
+});
+
+router.get("/", isLoggedIn, (req, res, next) => {
+  WishlistBook.find({ _id: req.WishlistBook._id })
+    .populate({
+      path: "wishlist",
     })
+    .then((res) => console.log(res))
+    .catch((err) => console.log(err));
+});
 
-    // router.get('/profile', (req, res, next) => {
-    //     const wlBookID = req.params.
-    
-    //     WishlistBook.findByIdAndDelete()
-    //     .then(deletedBook => {
-    //         console.log(deletedBook)
-    //         res.redirect('/profile')
-    //     })
-    // })
-    
-})
-
-router.get('/myWishList')
-
-module.exports = router
-
+module.exports = router;
